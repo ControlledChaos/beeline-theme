63bbaeebad0725946d5cc11921d2a1738ffe4929
ControlledChaos <greg@ccdzine.com>
2019-09-18 07:23:34 -0800

    Removed sidebar default widgets.

--- sidebar.php
+++ sidebar.php
@@ -8,48 +8,9 @@
  */
 
 ?>
-
 <aside id="secondary" class="widget-area">
 	<?php
-		if ( is_active_sidebar( 'sidebar' ) ) :
-
-		dynamic_sidebar( 'sidebar' );
-
-		else : ?>
-
-    	<?php get_search_form(); ?>
-
-    	<h3><?php _e( 'Archives', 'beeline-theme' ); ?></h3>
-    	<ul>
-    		<?php wp_get_archives( 'type=monthly' ); ?>
-		</ul>
-
-		<?php the_widget(
-			'WP_Widget_Categories',
-			null,
-			[
-				'before_title' => '<h3>',
-				'after_title' => '</h3>'
-			]
-		); ?>
-
-    	<h3><?php _e( 'Meta', 'beeline-theme' ); ?></h3>
-    	<ul>
-			<?php wp_register(); ?>
-			<?php if ( is_user_logged_in() ) : ?>
-			<li>
-				<a href="<?php echo get_edit_user_link(); ?>"><?php _e( 'Your Profile', 'beeline-theme' ); ?></a>
-			</li>
-			<?php endif; ?>
-    		<li><?php wp_loginout(); ?></li>
-			<?php wp_meta(); ?>
-    	</ul>
-
-    	<h3><?php _e( 'Subscribe', 'beeline-theme' ); ?></h3>
-    	<ul>
-    		<li><a href="<?php bloginfo( 'rss2_url' ); ?>"><?php _e( 'Entries RSS', 'beeline-theme' ); ?></a></li>
-    		<li><a href="<?php bloginfo( 'comments_rss2_url' ); ?>"><?php _e( 'Comments RSS', 'beeline-theme' ); ?></a></li>
-    	</ul>
-
-	<?php endif; ?>
+		if ( is_active_sidebar( 'sidebar' ) ) {
+			dynamic_sidebar( 'sidebar' );
+		} ?>
 </aside>
